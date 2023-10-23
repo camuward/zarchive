caec150a0996b6e7fa880362ea6819aecc2c0b6b
Cameron Ward <cameronuward@gmail.com>
2023-10-23 03:45:35 -0400

    remove proc macro crate

--- derive/src/validate.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro2::TokenStream;
-
-pub fn derive_valid(input: syn::DeriveInput) -> syn::Result<TokenStream> {
-    let syn::DeriveInput { attrs, vis, ident, generics, data } = input;
-    let syn::Generics { where_clause, .. } = generics;
-    
-    todo!()
-}
